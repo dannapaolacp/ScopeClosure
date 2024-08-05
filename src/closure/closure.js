48473079eb1a3ec2bb3fb67cba35ea3ebe0d550a
Danna Paola <dannacapera16@gmail.com>
2024-08-05 14:24:09 -0500

    Feat(closure): Playground two.

--- src/closure/closure.js
+++ src/closure/closure.js
@@ -12,3 +12,40 @@ const g = greeting();
 
 console.log(g); // Retornamos la definicion de la funcion
 console.log(g()); // Retornamos el valor, accede al ambito lexico y recuerda y contexto
+
+// Playground
+
+function createPetList() {
+  const petList = [];
+  return function addPet(myPet) {
+    if (myPet) {
+      petList.push(myPet);
+    }
+    return petList;
+  }
+}
+
+const myPetList = createPetList();
+
+myPetList("michi");
+
+myPetList("firulais");
+
+myPetList();
+
+console.log(myPetList());
+
+function createPetList() {
+  const pets = []
+  return (info) => info ? pets.push(info) : pets
+}
+
+const myPetList2 = createPetList();
+
+myPetList2("michi");
+
+myPetList2("firulais");
+
+myPetList2();
+
+console.log(myPetList2());
