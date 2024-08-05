77391f39a9eae6b90fdd4f4c07fcf099b59e263b
Danna Paola <dannacapera16@gmail.com>
2024-08-05 14:41:10 -0500

    Feat(hoisting): Hoisting variables and functions in JavaCcript.

---
+++ src/hoisting/index.js
@@ -0,0 +1,16 @@
+// variables
+var nameOfDog; // undefined
+console.log(nameOfDog);
+var nameOfDog = 'Miel';
+console.log(nameOfDog);
+
+
+// funciones
+var miel; // undefined
+nameOfDog2();
+
+function nameOfDog2() {
+  console.log(`El mejor perrito es ${miel}`);
+}
+
+var miel = 'Mielesita';
